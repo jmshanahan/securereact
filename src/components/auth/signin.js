f5c3923f5a68fc77c8d7ba7633e21cf0741c9f5f
jmshanahan <comeraghsolutions@gmail.com>
2018-04-03 11:37:57 +0100

    Signin form submit now works using Field attribute

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 
 class Signin extends Component {
-  handleFormSubmit(values){
-    console.log(values);
+  handleFormSubmit( {email, password}){
+    console.log(`Email is ${email} password is ${password}`);
   }
 
 
@@ -13,18 +13,35 @@ class Signin extends Component {
     // our form needs to listen to the onSubmit event
     // which comes from the handleSubmit and we pass that the function to handle it.
     // We need to bind it otherwise this is tied to the form.
-    const { handleSubmit, fields: {email, password}} = this.props;
+    const { handleSubmit } = this.props;
     return (
       <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-        <fieldset className="form-group">
-          <label>Email:</label>
-          <input {...email} className="form-control"/>
-        </fieldset>
-        <fieldset className="form-group">
-          <label>Password:</label>
-          <input {...password} className="form-control"/>
-        </fieldset> 
-        <button action="submit" className="btn btn-primary">Sign in</button>     
+        <div clsssName="form-group">
+        <label>Email</label>
+        <div>
+          <Field
+            name="email"
+            component="input"
+            type="text"
+            placeholder="Email"
+            className="form-control"
+          />
+        </div>
+      </div>
+      <div clsssName="form-group">
+        <label>Password </label>
+        <div>
+          <Field
+            name="password"
+            component="input"
+            type="text"
+            placeholder="Password"
+            className="form-control"
+
+          />
+        </div>
+        </div>
+        <button action="submit" className="btn btn-primary button">Sign in</button>     
       </form>
     );
   }
@@ -34,6 +51,5 @@ class Signin extends Component {
 // first set of params if for configuration
 // second set is for redux sign
 export default reduxForm({
-  form: 'signin',
-  fields: ['email', 'password']
+  form: 'signin'
 })(Signin);
